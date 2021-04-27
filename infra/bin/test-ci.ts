e9e0aed24eeed278dc4d44d1f02c85567f2a8813
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2021-04-27 15:02:39 -0700

    Install DNS and ALB controllers (#15)

--- infra/bin/test-ci.ts
+++ infra/bin/test-ci.ts
@@ -16,6 +16,7 @@ new TestCIStack(app, 'TestCIStack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
+  terminationProtection: true,
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
   clusterConfig: {
--- infra/lib/ci-cluster.ts
+++ infra/lib/ci-cluster.ts
@@ -1,10 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as eks from '@aws-cdk/aws-eks';
+import * as iam from '@aws-cdk/aws-iam';
 import * as cdk8s from 'cdk8s';
-import {FluxChart} from './charts/flux';
-import {FluxConfigurationChart} from './charts/flux-configuration';
-import {ProwSecretsChart, ProwSecretsChartProps} from './charts/prow-secrets';
-import {CommonNamespacesChart} from './charts/common-namespaces';
+import { policies as ALBPolicies } from './policies/aws-load-balancer-controller-policy';
+import { FluxChart } from './charts/flux';
+import { FluxConfigurationChart } from './charts/flux-configuration';
+import { ProwSecretsChart, ProwSecretsChartProps } from './charts/prow-secrets';
+import { NamespaceChart } from './charts/namespace';
+import { EXTERNAL_DNS_NAMESPACE, PROW_JOB_NAMESPACE, PROW_NAMESPACE } from './test-ci-stack';
 
 export type CIClusterCompileTimeProps = ProwSecretsChartProps;
 
@@ -15,36 +18,119 @@ export type CIClusterProps = CIClusterCompileTimeProps & CIClusterRuntimeProps;
 
 export class CICluster extends cdk.Construct {
   readonly testCluster: eks.Cluster;
+  readonly cdk8sApp: cdk8s.App = new cdk8s.App();
 
   constructor(scope: cdk.Construct, id: string, props: CIClusterProps) {
     super(scope, id);
 
-    this.testCluster = new eks.Cluster(scope, 'TestCluster', {
+    this.testCluster = new eks.Cluster(scope, 'TestInfraCluster', {
       version: eks.KubernetesVersion.V1_19,
     })
 
-    const commonNamespacesChart = this.testCluster.addCdk8sChart('common-namespaces',
-      new CommonNamespacesChart(new cdk8s.App(), 'CommonNamespaces', {}))
+    this.installProwRequirements(props);
+    this.installFlux();
+    this.installExternalDNS();
+    this.installAWSLoadBalancer();
+  }
 
-    const prowSecretsChart =
-      this.testCluster.addCdk8sChart('prow-secrets',
-        new ProwSecretsChart(
-          new cdk8s.App(), 'ProwSecrets', props
-        )
-      );
-    // Ensure namespaces are created before secrets
-    prowSecretsChart.node.addDependency(commonNamespacesChart);
+  createNamespace = (name: string) => {
+    return this.testCluster.addCdk8sChart(`${name}-namespace-chart`,
+      new NamespaceChart(this.cdk8sApp, `${name}Namespace`, {
+        name: name
+      }));
+  }
 
+  installFlux = () => {
     const fluxChart = this.testCluster.addCdk8sChart('flux',
       new FluxChart(
-        new cdk8s.App(), 'Flux', {}
+        this.cdk8sApp, 'Flux', {}
       )
     );
     const fluxConfigChart = this.testCluster.addCdk8sChart('flux-configuration',
       new FluxConfigurationChart(
-        new cdk8s.App(), 'FluxConfiguration', {}
+        this.cdk8sApp, 'FluxConfiguration', {}
       )
     );
     fluxConfigChart.node.addDependency(fluxChart);
+  }
+
+  installProwRequirements = (secretsProps: ProwSecretsChartProps) => {
+    let requiredNamespaces: eks.KubernetesManifest[] =
+      [PROW_NAMESPACE, PROW_JOB_NAMESPACE].map(this.createNamespace);
+
+    const prowSecretsChart =
+      this.testCluster.addCdk8sChart('prow-secrets',
+        new ProwSecretsChart(
+          this.cdk8sApp, 'ProwSecrets', secretsProps
+        )
+      );
+
+    // Ensure namespaces are created before secrets
+    prowSecretsChart.node.addDependency(...requiredNamespaces);
+  }
+
+  installExternalDNS = () => {
+    const externalDNSNamespace = this.createNamespace(EXTERNAL_DNS_NAMESPACE);
+
+    const externalDNSServiceAccount =
+      this.testCluster.addServiceAccount('external-dns-service-account', {
+        namespace: EXTERNAL_DNS_NAMESPACE,
+      });
+    externalDNSServiceAccount.node.addDependency(externalDNSNamespace);
+    externalDNSServiceAccount.addToPrincipalPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["route53:ChangeResourceRecordSets"],
+      resources: ["arn:aws:route53:::hostedzone/*"]
+    }))
+    externalDNSServiceAccount.addToPrincipalPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets"
+      ],
+      resources: ["*"]
+    }));
+
+    const helmChart = this.testCluster.addHelmChart('external-dns', {
+      chart: 'external-dns',
+      repository: 'https://charts.bitnami.com/bitnami',
+      namespace: EXTERNAL_DNS_NAMESPACE,
+      version: '4.11.1',
+      values: {
+        namespace: PROW_NAMESPACE, // Limit only to DNS in Prow
+        sources: ["ingress"],
+        policy: "upsert-only",
+        serviceAccount: {
+          create: false,
+          name: externalDNSServiceAccount.serviceAccountName
+        },
+        aws: {
+          zoneType: "public"
+        }
+      }
+    });
+    helmChart.node.addDependency(externalDNSNamespace);
+  }
+
+  installAWSLoadBalancer = () => {
+    const serviceAccount =
+      this.testCluster.addServiceAccount('alb-service-account', {
+        namespace: 'kube-system',
+      });
+    ALBPolicies.map(policy => serviceAccount.addToPrincipalPolicy(policy))
+
+    this.testCluster.addHelmChart('aws-load-balancer-controller', {
+      chart: 'aws-load-balancer-controller',
+      repository: 'https://aws.github.io/eks-charts',
+      namespace: 'kube-system',
+      version: '1.1.6',
+      values: {
+        clusterName: this.testCluster.clusterName,
+        serviceAccount: {
+          create: false,
+          name: serviceAccount.serviceAccountName
+        }
+      }
+    });
   }
 }
