15917150980b446663d7983e922433d669d79986
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2021-06-08 14:41:28 -0700

    Optional SSM agent and PVRE reporting (#51)
    Installs an SSM agent installer `DaemonSet` into the cluster that ensures all nodes have an Amazon SSM agent - taken from https://docs.aws.amazon.com/prescriptive-guidance/latest/patterns/install-ssm-agent-on-amazon-eks-worker-nodes-by-using-kubernetes-daemonset.html
    
    Creates an SSM association for all cluster instances and a respective SSM DataSync for instance inventory collection.

--- infra/lib/test-ci-stack.ts
+++ infra/lib/test-ci-stack.ts
@@ -1,13 +1,14 @@
 import * as cdk from '@aws-cdk/core';
 import { CICluster, CIClusterCompileTimeProps } from './ci-cluster';
 import { LogBucket, LogBucketCompileProps } from './log-bucket';
+import { ClusterSSM, ClusterSSMCompileProps } from './ssm';
 import { ProwServiceAccounts } from './prow-service-accounts';
 
 export const PROW_NAMESPACE = "prow";
 export const PROW_JOB_NAMESPACE = "test-pods";
 export const EXTERNAL_DNS_NAMESPACE = "external-dns";
 
-export type TestCIStackProps = cdk.StackProps & LogBucketCompileProps & {
+export type TestCIStackProps = cdk.StackProps & LogBucketCompileProps & ClusterSSMCompileProps & {
   clusterConfig: CIClusterCompileTimeProps
 };
 
@@ -23,6 +24,13 @@ export class TestCIStack extends cdk.Stack {
     const testCluster = new CICluster(this, 'CIClusterConstruct', {
       ...props.clusterConfig
     });
+
+    const clusterSSM = new ClusterSSM(this, 'CIClusterSSM', {
+      ...props,
+      account: this.account,
+      region: this.region,
+      cluster: testCluster.testCluster
+    })
 
     const prowServiceAccounts = new ProwServiceAccounts(this, 'ProwServiceAccountsConstruct', {
       account: this.account,
