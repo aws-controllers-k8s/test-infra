585cd7c904b092c382a0761d47b674252a6e9115
vijtrip2 <62766875+vijtrip2@users.noreply.github.com>
2021-10-01 14:33:29 -0700

    Add new postsubmit prowjob for auto tagging service controller repos

--- prow/jobs/images/push-image.sh
+++ prow/jobs/images/push-image.sh
@@ -16,8 +16,8 @@ Usage:
 Pushes all of the tagged 'prow/*' images into a public ECR repository. Use
 DOCKER_REPOSITORY to specify the ECR repository URI. Use VERSION to set the 
 SemVer value in the image tag.
-Valid IMAGE_TYPE are "deploy", "docs", "soak", "integration", "auto-generate-controllers"
-"olm-test" and "unit"
+Valid IMAGE_TYPE are "deploy", "docs", "soak", "integration", "auto-generate-controllers",
+"olm-test", "auto-tag" and "unit"
 
 Example:
 $(basename "$0") integration
