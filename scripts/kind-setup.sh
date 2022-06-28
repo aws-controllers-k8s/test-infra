f119a71cdf0dae750d4ffa02f6000cd3085e22d7
Nicholas Thomson <nithomso@amazon.com>
2022-06-28 23:18:07 +0000

    Update helm login with daws

--- scripts/kind-setup.sh
+++ scripts/kind-setup.sh
@@ -100,7 +100,7 @@ _install_additional_controller() {
 
 _perform_helm_login() {
   # ECR Public only exists in us-east-1 so use that region specifically
-  aws ecr-public get-login-password --region us-east-1 | helm registry login -u AWS --password-stdin public.ecr.aws
+  daws ecr-public get-login-password --region us-east-1 | helm registry login -u AWS --password-stdin public.ecr.aws
 }
 
 ensure_binaries() {
