7be909bd158738dcf35714923d0d5d596ffdf5d0
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2022-07-20 14:07:27 -0700

    Fix Prow web identity file passing (#234)
    Description of changes:
    Fixes the authentication issues for the Prow cluster's test scripts. As well as a bug in the container credentials file, the file was not properly being passed to the Pytest container underneath. This PR was tested against the production Prow cluster to ensure it works for our IRSA setup.
    
    Note: I have also removed the Prowjob's CPU requests, since there is no need for them to access 2 vCPUs worth of resources for every test run. This is unrelated to the web identity file work.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- prow/jobs/images/wrapper.sh
+++ prow/jobs/images/wrapper.sh
@@ -7,7 +7,7 @@
 # - starting / stopping docker-in-docker
 # -- configuring the docker daemon for IPv6
 # - ensuring GOPATH/bin is in PATH
-# - exporting and assuming ACK_ROLE_ARN
+# - exporting and assuming ACK_ASSUMED_ROLE_ARN
 #
 # After handling these things / before cleanup, my-test-command will be invoked,
 # and the exit code of my-test-command will be preserved by wrapper.sh
@@ -102,7 +102,7 @@ if git rev-parse --is-inside-work-tree >/dev/null; then
   # Use a reproducible build date based on the most recent git commit timestamp.
   SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
   export SOURCE_DATE_EPOCH
-  >&2 echo "wrapper.sh] [SETUP] exported SOURCE_DATE_EPOCH=${SOURCE_DATE_EPOCH}"
+  >&2 echo "wrapper.sh] [SETUP] Exported SOURCE_DATE_EPOCH=${SOURCE_DATE_EPOCH}"
 fi
 
 >&2 echo "wrapper.sh] [SETUP] Logging into ECR public ..."
@@ -110,17 +110,17 @@ aws ecr-public get-login-password --region us-east-1 | docker login --username A
 >&2 echo "wrapper.sh] [SETUP] Logged in"
 
 ASSUME_EXIT_VALUE=0
-ACK_ROLE_ARN=$(aws ssm get-parameter --name /ack/prow/service_team_role/$AWS_SERVICE --query Parameter.Value --output text 2>/dev/null) || ASSUME_EXIT_VALUE=$?
+ACK_ASSUMED_ROLE_ARN=$(aws ssm get-parameter --name /ack/prow/service_team_role/$AWS_SERVICE --query Parameter.Value --output text 2>/dev/null) || ASSUME_EXIT_VALUE=$?
 if [ "$ASSUME_EXIT_VALUE" -ne 0 ]; then
   >&2 echo "wrapper.sh] [SETUP] Could not find service team role for $AWS_SERVICE"
   exit 1
 fi
-export ACK_ROLE_ARN
->&2 echo "wrapper.sh] [SETUP] exported ACK_ROLE_ARN"
+export ACK_ASSUMED_ROLE_ARN
+>&2 echo "wrapper.sh] [SETUP] Exported ACK_ASSUMED_ROLE_ARN"
 
-ASSUME_COMMAND=$(aws sts assume-role --role-arn $ACK_ROLE_ARN --role-session-name $PROW_JOB_ID --duration-seconds 3600 | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+ASSUME_COMMAND=$(aws sts assume-role --role-arn $ACK_ASSUMED_ROLE_ARN --role-session-name $PROW_JOB_ID --duration-seconds 3600 | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 eval $ASSUME_COMMAND
->&2 echo "wrapper.sh] [SETUP] Assumed ACK_ROLE_ARN"
+>&2 echo "wrapper.sh] [SETUP] Assumed ACK_ASSUMED_ROLE_ARN"
 
 # actually run the user supplied command
 printf '%0.s=' {1..80}; echo
--- scripts/pytest-image-runner.sh
+++ scripts/pytest-image-runner.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # pytest-image-runner.sh contains functions used to build and run the ACK Python
-# test framework from within a container.
+# test framework from within a container. When building the test container, it
+# places certain files into pre-defined locations such that the Dockerfile can
+# copy them into the image. At runtime, it sets environment variables indicating
+# where in the image those files were copied. Any variable beginning with
+# `TMP_TEST` indicates the location of a temporary file for build-item. Any
+# variable beginning with `TEST_CONTAINER` indicates the location within the
+# container into which a file has been copied.
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="$SCRIPTS_DIR/.."
@@ -17,14 +23,18 @@ SERVICE_CONTROLLER_E2E_TEST_PATH="${SERVICE_CONTROLLER_E2E_TEST_PATH:-$DEFAULT_S
 # The location of new credential file which will be copied in test container
 # Keep this file in same location as '~/.aws/credentials' file
 TMP_TEST_AWS_CREDS_FILE_LOCATION="$HOME/.aws/ack-test-credentials"
-# The name of the AWS profile inside the container for rotating credentials
-TEST_AWS_PROFILE_NAME="ack-test"
 # The file path containing a copy of the test config
 TMP_TEST_CONFIG_FILE_LOCATION="$HOME/.aws/$AWS_SERVICE-test-config.yaml"
 
+# The name of the AWS profile inside the container for rotating credentials
+TEST_AWS_PROFILE_NAME="ack-test"
+
+# Path of web-identity-token in test container
+TEST_CONTAINER_WEB_IDENTITY_TOKEN_FILE="/root/web-identity-token"
+
 # The following environment variables are injected when running as a Prow job
 PROW_JOB_ID=${PROW_JOB_ID:-}
-AWS_WEB_IDENTITY_TOKEN_FILE=${AWS_WEB_IDENTITY_TOKEN_FILE:-}
+AWS_ROLE_ARN=${AWS_ROLE_ARN:-}
 
 source "$SCRIPTS_DIR/lib/common.sh"
 source "$SCRIPTS_DIR/lib/config.sh"
@@ -33,24 +43,23 @@ source "$SCRIPTS_DIR/lib/logging.sh"
 build_pytest_image() {
     local __image_tag=$1
 
-    local ack_role_arn=$(get_assumed_role_arn)
+    local assumed_role_arn=$(get_assumed_role_arn)
+    local identity_file="$(get_aws_token_file)"
 
-    # Path of web-identity-token in test container
-    local test_container_web_identity_token_file="/root/web-identity-token"
     # If 'AWS_PROFILE' variable is set, use it as source profile for 'ack-test'
     # profile. Use 'default' as fallback
-    local ack_test_source_aws_profile=${AWS_PROFILE:-"default"}
+    local ack_test_source_aws_profile=${get_aws_profile:-"default"}
     local aws_creds_file_location="$HOME/.aws/credentials"
 
     if [[ -n $PROW_JOB_ID ]]; then
         # If this is prowjob, create new aws credentials file and setup two profiles
         # 1. 'prow-irsa' profile which gets aws credentials using web-identity-token
         # 2. 'ack-test' profile which uses 'prow-irsa' as source profile and assumes
-        # ack_role_arn for aws credentials
+        # assumed_role_arn for aws credentials
         # NOTE: credentials in both these profiles rotate automatically
 
         # copy web-identity-token file for use inside test container
-        cp "$AWS_WEB_IDENTITY_TOKEN_FILE" "$SCRIPTS_DIR"/web-identity-token >/dev/null
+        cp "$identity_file" "$SCRIPTS_DIR"/web-identity-token >/dev/null
 
         # generate new aws-credentials file for test container
         eval "echo \"$(cat "$SCRIPTS_DIR/creds-templates/prow-test-aws-creds-template.txt")\"" > "$TMP_TEST_AWS_CREDS_FILE_LOCATION"
@@ -76,7 +85,7 @@ build_pytest_image() {
         local test_docker_sha="$(docker build --file "${e2e_test_dockerfile}" \
         --tag $__image_tag \
         --build-arg AWS_SERVICE="${AWS_SERVICE}" \
-        --build-arg WEB_IDENTITY_TOKEN_DEST_PATH="${test_container_web_identity_token_file}" \
+        --build-arg WEB_IDENTITY_TOKEN_DEST_PATH="${TEST_CONTAINER_WEB_IDENTITY_TOKEN_FILE}" \
         --quiet . )"
         debug_msg "Built PyTest image $__image_tag ($test_docker_sha)"
     popd 1>/dev/null
@@ -86,23 +95,31 @@ run_pytest_image() {
     local __image_tag=$1
 
     local region=$(get_aws_region)
+    local identity_file="$(get_aws_token_file)"
 
     # Copy the test config into the temporary path
     cp "$(get_test_config_path)" $TMP_TEST_CONFIG_FILE_LOCATION
 
     info_msg "Running e2e test container for $AWS_SERVICE ..."
 
+    params=()
+    if [[ "$identity_file" != "" ]]; then
+        params+=(-e AWS_WEB_IDENTITY_TOKEN_FILE="$TEST_CONTAINER_WEB_IDENTITY_TOKEN_FILE")
+    fi
+
     docker run --rm -t \
         --network="host" \
         -v "$KUBECONFIG":/root/.kube/config:z \
         -v "$TMP_TEST_AWS_CREDS_FILE_LOCATION":/root/.aws/credentials:z \
         -v "$TMP_TEST_CONFIG_FILE_LOCATION":/root/test-config.yaml:z \
+        -e TEST_CONFIG_PATH="/root/test-config.yaml" \
         -e SERVICE_CONTROLLER_E2E_TEST_PATH="." \
-        -e ACK_ROLE_ARN \
+        -e ASSUMED_ROLE_ARN \
         -e PYTEST_LOG_LEVEL \
         -e PYTEST_NUM_THREADS \
         -e AWS_DEFAULT_REGION="$region" \
         -e AWS_PROFILE="$TEST_AWS_PROFILE_NAME" \
+        "${params[@]}" \
         "$__image_tag"
 }
 
