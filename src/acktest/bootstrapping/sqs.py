55b04d9c55cdc3245e2f6c7a9c57204e08097c01
Amine Hilaly <hilalyamine@gmail.com>
2022-08-04 20:14:13 +0200

    Add SQS `Queue` bootsrappable resource

---
+++ src/acktest/bootstrapping/sqs.py
@@ -0,0 +1,60 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+#	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import boto3
+
+from dataclasses import dataclass, field
+
+from . import Bootstrappable
+from .. import resources
+
+@dataclass
+class Queue(Bootstrappable):
+    # Inputs
+    name_prefix: str
+    policy: str = ""
+    
+    # Outputs
+    arn: str = field(init=False)
+    
+    @property
+    def sqs_client(self):
+        return boto3.client("sqs", region_name=self.region)
+
+    @property
+    def sqs_resource(self):
+        return boto3.resource("sqs", region_name=self.region)
+
+    def bootstrap(self):
+        """Creates an SQS queue with an auto-generated name.
+        """
+        self.name = resources.random_suffix_name(self.name_prefix, 63)
+
+        create_attributes = {}
+
+        if self.policy != "":
+            create_attributes["Policy"] = self.policy
+
+        queue = self.sqs_resource.create_queue(
+            QueueName=self.name,
+            Attributes=create_attributes,
+        )
+        self.url = queue.url
+        self.arn = queue.attributes["QueueArn"]
+
+    def cleanup(self):
+        """Deletes an SQS queue.
+        """
+        self.sqs_client.delete_queue(
+            QueueUrl=self.url,
+        )
