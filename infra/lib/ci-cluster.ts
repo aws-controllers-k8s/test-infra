6f5225aff62d4ea6c3ba82f041aeb4dd9d4c1596
Nicholas Thomson <nithomso@amazon.com>
2022-12-08 21:21:41 +0000

    Add explicit manifest dependencies

--- infra/lib/ci-cluster.ts
+++ infra/lib/ci-cluster.ts
@@ -1,25 +1,31 @@
-import * as cdk from '@aws-cdk/core';
-import * as eks from '@aws-cdk/aws-eks';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as iam from '@aws-cdk/aws-iam';
-import * as cdk8s from 'cdk8s';
-import { policies as ALBPolicies } from './policies/aws-load-balancer-controller-policy';
-import { FluxConfigurationChart } from './charts/flux-configuration';
-import { ProwSecretsChart, ProwSecretsChartProps } from './charts/prow-secrets';
-import { NamespaceChart } from './charts/namespace';
-import { EXTERNAL_DNS_NAMESPACE, FLUX_NAMESPACE, PROW_JOB_NAMESPACE, PROW_NAMESPACE } from './test-ci-stack';
+import * as path from "path";
+import * as cdk from "@aws-cdk/core";
+import * as eks from "@aws-cdk/aws-eks";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as iam from "@aws-cdk/aws-iam";
+import * as cdk8s from "cdk8s";
+import { policies as ALBPolicies } from "./policies/aws-load-balancer-controller-policy";
+import { ProwSecretsChart, ProwSecretsChartProps } from "./charts/prow-secrets";
+import {
+  EXTERNAL_DNS_NAMESPACE,
+  FLUX_NAMESPACE,
+  PROW_JOB_NAMESPACE,
+  PROW_NAMESPACE,
+} from "./test-ci-stack";
+import { KubernetesManifest } from "@aws-cdk/aws-eks";
+import { Chart } from "cdk8s";
 
 export type CIClusterCompileTimeProps = ProwSecretsChartProps;
 
-export type CIClusterRuntimeProps = {
-};
+export type CIClusterRuntimeProps = {};
 
 export type CIClusterProps = CIClusterCompileTimeProps & CIClusterRuntimeProps;
 
 export class CICluster extends cdk.Construct {
   readonly testCluster: eks.Cluster;
   readonly testNodegroup: eks.Nodegroup;
-  readonly cdk8sApp: cdk8s.App = new cdk8s.App();
+
+  readonly namespaceManifests: eks.KubernetesManifest[];
 
   constructor(scope: cdk.Construct, id: string, props: CIClusterProps) {
     super(scope, id);
@@ -36,114 +42,161 @@ export class CICluster extends cdk.Construct {
 
     this.installProwRequirements(props);
     this.installFlux();
-    this.installFluxConfiguration();
     this.installExternalDNS();
     this.installAWSLoadBalancer();
   }
 
   createNamespace = (name: string) => {
-    return this.testCluster.addCdk8sChart(`${name}-namespace-chart`,
-      new NamespaceChart(this.cdk8sApp, `${name}Namespace`, {
-        name: name
-      }));
-  }
+    return new KubernetesManifest(
+      this.testCluster.stack,
+      `${name}-namespace-struct`,
+      {
+        cluster: this.testCluster,
+        manifest: [
+          {
+            apiVersion: "v1",
+            kind: "Namespace",
+            metadata: {
+              name: name,
+            },
+          },
+        ],
+      }
+    );
+  };
 
   installFlux = () => {
-    const fluxChart = this.testCluster.addHelmChart('flux2', {
-      chart: 'flux2',
-      repository: 'https://fluxcd-community.github.io/helm-charts',
+    const fluxChart = this.testCluster.addHelmChart("flux2", {
+      chart: "flux2",
+      repository: "https://fluxcd-community.github.io/helm-charts",
       namespace: FLUX_NAMESPACE,
       createNamespace: true,
-      version: '0.19.2',
+      version: "0.19.2",
       values: {},
-    })
-  }
+    });
 
-  installFluxConfiguration = () => {
-    const fluxConfigChart = this.testCluster.addCdk8sChart('flux-configuration',
-      new FluxConfigurationChart(
-        this.cdk8sApp, 'FluxConfiguration', {}
-      )
+    const fluxBootstrap = this.testCluster.addManifest(
+      "FluxBootstrap",
+      ...[
+        {
+          apiVersion: "source.toolkit.fluxcd.io/v1beta2",
+          kind: "GitRepository",
+          metadata: {
+            name: "test-infra",
+            namespace: "flux-system",
+          },
+          spec: {
+            interval: "30s",
+            ref: {
+              branch: "main",
+            },
+            url: "https://github.com/aws-controllers-k8s/test-infra",
+          },
+        },
+        {
+          apiVersion: "kustomize.toolkit.fluxcd.io/v1beta2",
+          kind: "Kustomization",
+          metadata: {
+            name: "all-apps",
+            namespace: "flux-system",
+          },
+          spec: {
+            interval: "5m",
+            sourceRef: {
+              kind: "GitRepository",
+              name: "test-infra",
+            },
+            path: "./flux",
+            prune: true,
+            targetNamespace: "flux-system",
+            validation: "client",
+          },
+        },
+      ]
     );
-  }
+    fluxBootstrap.node.addDependency(fluxChart);
+  };
 
   installProwRequirements = (secretsProps: ProwSecretsChartProps) => {
-    let requiredNamespaces: eks.KubernetesManifest[] =
-      [PROW_NAMESPACE, PROW_JOB_NAMESPACE].map(this.createNamespace);
-
-    const prowSecretsChart =
-      this.testCluster.addCdk8sChart('prow-secrets',
-        new ProwSecretsChart(
-          this.cdk8sApp, 'ProwSecrets', secretsProps
-        )
-      );
+    const prowSecretsApp = new cdk8s.App();
+    const prowSecretsChart = this.testCluster.addCdk8sChart(
+      "prow-secrets",
+      new ProwSecretsChart(prowSecretsApp, "ProwSecrets", secretsProps)
+    );
 
     // Ensure namespaces are created before secrets
-    prowSecretsChart.node.addDependency(...requiredNamespaces);
-  }
+    prowSecretsChart.node.addDependency(...this.namespaceManifests);
+    prowSecretsApp.charts.forEach((chart) =>
+      chart.addDependency(...this.namespaceManifests)
+    );
+  };
 
   installExternalDNS = () => {
-    const externalDNSNamespace = this.createNamespace(EXTERNAL_DNS_NAMESPACE);
-
-    const externalDNSServiceAccount =
-      this.testCluster.addServiceAccount('external-dns-service-account', {
+    const externalDNSServiceAccount = this.testCluster.addServiceAccount(
+      "external-dns-service-account",
+      {
         namespace: EXTERNAL_DNS_NAMESPACE,
-      });
-    externalDNSServiceAccount.node.addDependency(externalDNSNamespace);
-    externalDNSServiceAccount.addToPrincipalPolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: ["route53:ChangeResourceRecordSets"],
-      resources: ["arn:aws:route53:::hostedzone/*"]
-    }))
-    externalDNSServiceAccount.addToPrincipalPolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: [
-        "route53:ListHostedZones",
-        "route53:ListResourceRecordSets"
-      ],
-      resources: ["*"]
-    }));
+      }
+    );
+    externalDNSServiceAccount.node.addDependency(...this.namespaceManifests);
 
-    const helmChart = this.testCluster.addHelmChart('external-dns', {
-      chart: 'external-dns',
-      repository: 'https://charts.bitnami.com/bitnami',
+    externalDNSServiceAccount.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["route53:ChangeResourceRecordSets"],
+        resources: ["arn:aws:route53:::hostedzone/*"],
+      })
+    );
+    externalDNSServiceAccount.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["route53:ListHostedZones", "route53:ListResourceRecordSets"],
+        resources: ["*"],
+      })
+    );
+
+    const helmChart = this.testCluster.addHelmChart("external-dns", {
+      chart: "external-dns",
+      repository: "https://charts.bitnami.com/bitnami",
       namespace: EXTERNAL_DNS_NAMESPACE,
-      version: '6.12.0',
+      version: "6.12.0",
       values: {
         namespace: PROW_NAMESPACE, // Limit only to DNS in Prow
         sources: ["ingress"],
         policy: "upsert-only",
         serviceAccount: {
           create: false,
-          name: externalDNSServiceAccount.serviceAccountName
+          name: externalDNSServiceAccount.serviceAccountName,
         },
         aws: {
-          zoneType: "public"
-        }
-      }
+          zoneType: "public",
+        },
+      },
     });
-    helmChart.node.addDependency(externalDNSNamespace);
-  }
+    helmChart.node.addDependency(...this.namespaceManifests);
+  };
 
   installAWSLoadBalancer = () => {
-    const serviceAccount =
-      this.testCluster.addServiceAccount('alb-service-account', {
-        namespace: 'kube-system',
-      });
-    ALBPolicies.map(policy => serviceAccount.addToPrincipalPolicy(policy))
+    const serviceAccount = this.testCluster.addServiceAccount(
+      "alb-service-account",
+      {
+        namespace: "kube-system",
+      }
+    );
+    ALBPolicies.map((policy) => serviceAccount.addToPrincipalPolicy(policy));
 
-    this.testCluster.addHelmChart('aws-load-balancer-controller', {
-      chart: 'aws-load-balancer-controller',
-      repository: 'https://aws.github.io/eks-charts',
-      namespace: 'kube-system',
-      version: '1.1.6',
+    this.testCluster.addHelmChart("aws-load-balancer-controller", {
+      chart: "aws-load-balancer-controller",
+      repository: "https://aws.github.io/eks-charts",
+      namespace: "kube-system",
+      version: "1.1.6",
       values: {
         clusterName: this.testCluster.clusterName,
         serviceAccount: {
           create: false,
-          name: serviceAccount.serviceAccountName
-        }
-      }
+          name: serviceAccount.serviceAccountName,
+        },
+      },
     });
-  }
+  };
 }
--- infra/lib/prow-service-accounts.ts
+++ infra/lib/prow-service-accounts.ts
@@ -1,8 +1,8 @@
-import * as cdk from '@aws-cdk/core';
-import * as eks from '@aws-cdk/aws-eks';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as iam from '@aws-cdk/aws-iam';
-import { PROW_JOB_NAMESPACE, PROW_NAMESPACE } from './test-ci-stack';
+import * as cdk from "@aws-cdk/core";
+import * as eks from "@aws-cdk/aws-eks";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as iam from "@aws-cdk/aws-iam";
+import { PROW_JOB_NAMESPACE, PROW_NAMESPACE } from "./test-ci-stack";
 
 export type ProwServiceAccountsProps = {
   account: string;
@@ -10,6 +10,8 @@ export type ProwServiceAccountsProps = {
   region: string;
 
   prowCluster: eks.Cluster;
+  namespaceManifests: eks.KubernetesManifest[];
+
   tideStatusBucket: s3.Bucket;
   presubmitsBucket: s3.Bucket;
   postsubmitsBucket: s3.Bucket;
@@ -20,7 +22,11 @@ export class ProwServiceAccounts extends cdk.Construct {
   readonly presubmitJobServiceAccount: eks.ServiceAccount;
   readonly postsubmitJobServiceAccount: eks.ServiceAccount;
 
-  constructor(scope: cdk.Construct, id: string, props: ProwServiceAccountsProps) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: ProwServiceAccountsProps
+  ) {
     super(scope, id);
 
     // Necessary only when splitting control and data plane
@@ -28,7 +34,7 @@ export class ProwServiceAccounts extends cdk.Construct {
     //   actions: ["eks:DescribeCluster"],
     //   resources: [props.prowCluster.clusterArn],
     // });
-  
+
     const tideStatusReconcilerAccessPolicy = new iam.PolicyStatement({
       actions: ["s3:Get*", "s3:List*", "s3:Put*", "s3:DeleteObject"],
       resources: [
@@ -39,13 +45,13 @@ export class ProwServiceAccounts extends cdk.Construct {
 
     const preAssumeRolePolicy = new iam.PolicyStatement({
       actions: ["sts:AssumeRole"],
-      resources: ["*"]
+      resources: ["*"],
     });
 
     // Used to validate recommended-policy-arn in service controllers repository
     const preGetPolicyPolicy = new iam.PolicyStatement({
       actions: ["iam:GetPolicy"],
-      resources: ["*"]
+      resources: ["*"],
     });
 
     const preBucketAccessPolicy = new iam.PolicyStatement({
@@ -74,10 +80,10 @@ export class ProwServiceAccounts extends cdk.Construct {
         "ecr-public:DescribeImages",
         "ecr-public:DescribeImageTags",
         "ecr-public:GetRepositoryCatalogData",
-        "ecr-public:GetRegistryCatalogData"
+        "ecr-public:GetRegistryCatalogData",
       ],
-      resources: ["*"]
-    })
+      resources: ["*"],
+    });
 
     const postBucketAccessPolicy = new iam.PolicyStatement({
       actions: ["s3:Get*", "s3:List*", "s3:Put*", "s3:DeleteObject"],
@@ -101,7 +107,7 @@ export class ProwServiceAccounts extends cdk.Construct {
         "ecr-public:PutImage",
         "ecr-public:ListTagsForResource",
         "ecr-public:PutRegistryCatalogData",
-        "ecr-public:BatchCheckLayerAvailability"
+        "ecr-public:BatchCheckLayerAvailability",
       ],
       resources: [
         `arn:${props.stackPartition}:ecr-public::${props.account}:registry/*`,
@@ -111,22 +117,18 @@ export class ProwServiceAccounts extends cdk.Construct {
 
     const postEcrPublicAllResourcePolicy = new iam.PolicyStatement({
       actions: ["ecr-public:GetAuthorizationToken"],
-      resources: [
-        "*",
-      ],
+      resources: ["*"],
     });
 
     const postStsPolicy = new iam.PolicyStatement({
       actions: ["sts:GetServiceBearerToken"],
-      resources: [
-        "*",
-      ],
+      resources: ["*"],
     });
 
     // Assumes the Role in service team's account to access soak EKS cluster
     const postAssumeRolePolicy = new iam.PolicyStatement({
       actions: ["sts:AssumeRole"],
-      resources: ["*"]
+      resources: ["*"],
     });
 
     const postParamStoreAccessPolicy = new iam.PolicyStatement({
@@ -138,56 +140,85 @@ export class ProwServiceAccounts extends cdk.Construct {
 
     // Service account for each of the Prow deployments
     // TODO(RedbackThomson): Split by service and assign individual permissions to each
-    this.deploymentServiceAccount = props.prowCluster.addServiceAccount('ProwDeploymentServiceAccount', {
-      namespace: PROW_NAMESPACE,
-      name: "prow-deployment-service-account"
-    });
+    this.deploymentServiceAccount = props.prowCluster.addServiceAccount(
+      "ProwDeploymentServiceAccount",
+      {
+        namespace: PROW_NAMESPACE,
+        name: "prow-deployment-service-account",
+      }
+    );
+    this.deploymentServiceAccount.node.addDependency(
+      ...props.namespaceManifests
+    );
 
     // this.deploymentServiceAccount.addToPrincipalPolicy(dataplaneAccessPolicy);
-    this.deploymentServiceAccount.addToPrincipalPolicy(tideStatusReconcilerAccessPolicy);
+    this.deploymentServiceAccount.addToPrincipalPolicy(
+      tideStatusReconcilerAccessPolicy
+    );
     this.deploymentServiceAccount.addToPrincipalPolicy(preBucketAccessPolicy);
     this.deploymentServiceAccount.addToPrincipalPolicy(postBucketAccessPolicy);
 
-    new cdk.CfnOutput(scope, 'DeploymentServiceAccountRoleOutput', {
+    new cdk.CfnOutput(scope, "DeploymentServiceAccountRoleOutput", {
       value: this.deploymentServiceAccount.role.roleName,
-      exportName: 'DeploymentServiceAccountRoleName',
-      description: 'Role ARN for the Prow deployments service account'
+      exportName: "DeploymentServiceAccountRoleName",
+      description: "Role ARN for the Prow deployments service account",
     });
 
     // Service account for presubmit jobs
-    this.presubmitJobServiceAccount = props.prowCluster.addServiceAccount('PreSubmitJobServiceAccount', {
-      namespace: PROW_JOB_NAMESPACE,
-      name: "pre-submit-service-account"
-    });
+    this.presubmitJobServiceAccount = props.prowCluster.addServiceAccount(
+      "PreSubmitJobServiceAccount",
+      {
+        namespace: PROW_JOB_NAMESPACE,
+        name: "pre-submit-service-account",
+      }
+    );
+    this.presubmitJobServiceAccount.node.addDependency(
+      ...props.namespaceManifests
+    );
 
     this.presubmitJobServiceAccount.addToPrincipalPolicy(preAssumeRolePolicy);
     this.presubmitJobServiceAccount.addToPrincipalPolicy(preGetPolicyPolicy);
     this.presubmitJobServiceAccount.addToPrincipalPolicy(preBucketAccessPolicy);
-    this.presubmitJobServiceAccount.addToPrincipalPolicy(preParamStoreAccessPolicy);
-    this.presubmitJobServiceAccount.addToPrincipalPolicy(preECRPublicReadOnlyPolicy)
+    this.presubmitJobServiceAccount.addToPrincipalPolicy(
+      preParamStoreAccessPolicy
+    );
+    this.presubmitJobServiceAccount.addToPrincipalPolicy(
+      preECRPublicReadOnlyPolicy
+    );
 
-    new cdk.CfnOutput(scope, 'PreSubmitServiceAccountRoleOutput', {
+    new cdk.CfnOutput(scope, "PreSubmitServiceAccountRoleOutput", {
       value: this.presubmitJobServiceAccount.role.roleName,
-      exportName: 'PreSubmitServiceAccountRoleName',
-      description: 'Role ARN for the Prow presubmit jobs\' service account'
+      exportName: "PreSubmitServiceAccountRoleName",
+      description: "Role ARN for the Prow presubmit jobs' service account",
     });
 
     // Service account for postsubmit jobs
-    this.postsubmitJobServiceAccount = props.prowCluster.addServiceAccount('PostSubmitJobServiceAccount', {
-      namespace: PROW_JOB_NAMESPACE,
-      name: "post-submit-service-account"
-    });
-    this.postsubmitJobServiceAccount.addToPrincipalPolicy(postBucketAccessPolicy);
+    this.postsubmitJobServiceAccount = props.prowCluster.addServiceAccount(
+      "PostSubmitJobServiceAccount",
+      {
+        namespace: PROW_JOB_NAMESPACE,
+        name: "post-submit-service-account",
+      }
+    );
+    this.postsubmitJobServiceAccount.node.addDependency(
+      ...props.namespaceManifests
+    );
+    this.postsubmitJobServiceAccount.addToPrincipalPolicy(
+      postBucketAccessPolicy
+    );
     this.postsubmitJobServiceAccount.addToPrincipalPolicy(postEcrPublicPolicy);
-    this.postsubmitJobServiceAccount.addToPrincipalPolicy(postEcrPublicAllResourcePolicy);
+    this.postsubmitJobServiceAccount.addToPrincipalPolicy(
+      postEcrPublicAllResourcePolicy
+    );
     this.postsubmitJobServiceAccount.addToPrincipalPolicy(postStsPolicy);
     this.postsubmitJobServiceAccount.addToPrincipalPolicy(postAssumeRolePolicy);
-    this.postsubmitJobServiceAccount.addToPrincipalPolicy(postParamStoreAccessPolicy);
-    new cdk.CfnOutput(scope, 'PostSubmitServiceAccountRoleOutput', {
+    this.postsubmitJobServiceAccount.addToPrincipalPolicy(
+      postParamStoreAccessPolicy
+    );
+    new cdk.CfnOutput(scope, "PostSubmitServiceAccountRoleOutput", {
       value: this.postsubmitJobServiceAccount.role.roleName,
-      exportName: 'PostSubmitServiceAccountRoleName',
-      description: 'Role ARN for the Prow postsubmit jobs\' service account'
+      exportName: "PostSubmitServiceAccountRoleName",
+      description: "Role ARN for the Prow postsubmit jobs' service account",
     });
-
   }
 }
