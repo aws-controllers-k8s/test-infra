14dda81ce8ea430b51c9ce738483bea3744a5450
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2023-01-23 07:20:10 -0800

    Update test image Go versions to 1.19 (#285)
    Description of changes:
    Updates each of the Prow job images that require Go to 1.19 and updates the Prow job image tags to use the latest pushed versions.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- prow/jobs/images/build-images.sh
+++ prow/jobs/images/build-images.sh
@@ -22,20 +22,24 @@ IMAGE_DIR=$DIR
 
 QUIET=${QUIET:-"false"}
 
+GO_VERSION=${GO_VERSION:-"1.19"}
+
 # check_is_installed docker
 
-docker build -f "$IMAGE_DIR/Dockerfile.deploy" --quiet=$QUIET -t "prow/deploy" "${IMAGE_DIR}"
-docker build -f "$IMAGE_DIR/Dockerfile.docs" --quiet=$QUIET -t "prow/docs" "${IMAGE_DIR}"
-docker build -f "$IMAGE_DIR/Dockerfile.olm-bundle-pr" --quiet=$QUIET -t "prow/olm-bundle-pr" "${IMAGE_DIR}"
-docker build -f "$IMAGE_DIR/Dockerfile.olm-test" --quiet=$QUIET -t "prow/olm-test" "${IMAGE_DIR}"
-docker build -f "$IMAGE_DIR/Dockerfile.auto-generate-controllers" --quiet=$QUIET -t "prow/auto-generate-controllers" "${IMAGE_DIR}"
-docker build -f "$IMAGE_DIR/Dockerfile.auto-update-controllers" --quiet=$QUIET -t "prow/auto-update-controllers" "${IMAGE_DIR}"
-docker build -f "$IMAGE_DIR/Dockerfile.controller-release-tag" --quiet=$QUIET -t "prow/controller-release-tag" "${IMAGE_DIR}"
-docker build -f "$IMAGE_DIR/Dockerfile.soak" --quiet=$QUIET --build-arg DEPLOY_BASE_TAG="prow/deploy" -t "prow/soak" "${IMAGE_DIR}"
+docker_build_args=( "--quiet=$QUIET" "--build-arg=GO_VERSION=$GO_VERSION" )
+
+docker build -f "$IMAGE_DIR/Dockerfile.deploy" "${docker_build_args[@]}" -t "prow/deploy" "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.docs" "${docker_build_args[@]}" -t "prow/docs" "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.olm-bundle-pr" "${docker_build_args[@]}" -t "prow/olm-bundle-pr" "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.olm-test" "${docker_build_args[@]}" -t "prow/olm-test" "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.auto-generate-controllers" "${docker_build_args[@]}" -t "prow/auto-generate-controllers" "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.auto-update-controllers" "${docker_build_args[@]}" -t "prow/auto-update-controllers" "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.controller-release-tag" "${docker_build_args[@]}" -t "prow/controller-release-tag" "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.soak" "${docker_build_args[@]}" --build-arg DEPLOY_BASE_TAG="prow/deploy" -t "prow/soak" "${IMAGE_DIR}"
 
 export TEST_BASE_TAG=$(echo "prow/test-$(uuidgen | cut -c1-8)" | tr '[:upper:]' '[:lower:]')
-docker build -f "$IMAGE_DIR/Dockerfile.test" --quiet=$QUIET -t $TEST_BASE_TAG "${IMAGE_DIR}"
+docker build -f "$IMAGE_DIR/Dockerfile.test" "${docker_build_args[@]}" -t $TEST_BASE_TAG "${IMAGE_DIR}"
 
 for IMAGE_TYPE in integration unit; do
-  docker build -f "$IMAGE_DIR/Dockerfile.$IMAGE_TYPE" --quiet=$QUIET --build-arg TEST_BASE_TAG -t "prow/$IMAGE_TYPE" "${IMAGE_DIR}"
+  docker build -f "$IMAGE_DIR/Dockerfile.$IMAGE_TYPE" "${docker_build_args[@]}" --build-arg TEST_BASE_TAG -t "prow/$IMAGE_TYPE" "${IMAGE_DIR}"
 done
