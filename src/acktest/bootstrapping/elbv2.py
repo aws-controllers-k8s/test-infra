1ec4bcee4c2d53eac564e02fb7b695e32a40d00c
Abhinav Khanna <abhinav.khanna@instabase.com>
2023-03-09 09:13:16 -0600

    feat(elbv2): elbv2 bootstrap added

---
+++ src/acktest/bootstrapping/elbv2.py
@@ -0,0 +1,77 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+#	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+import time
+
+import boto3
+
+from dataclasses import dataclass, field
+
+from .. import resources
+from . import Bootstrappable
+from e2e.bootstrap_resources import get_bootstrap_resources
+
+
+@dataclass
+class NetworkLoadBalancer(Bootstrappable):
+  # Inputs
+  name_prefix: str
+  ip_address_type: str = "ipv4"
+  type: str = "network"
+  scheme: str = "internet-facing"
+
+  # Subresources
+
+  # Outputs
+  arn: str = field(init=False)
+
+  @property
+  def elbv2_client(self):
+    return boto3.client("elbv2", region_name=self.region)
+
+  @property
+  def elbv2_resource(self):
+    return boto3.resource("elbv2", region_name=self.region)
+
+  def bootstrap(self):
+    """Creates a Network Load Balancer cluster with an auto-generated name.
+    """
+    super().bootstrap()
+
+    self.name = resources.random_suffix_name(self.name_prefix, 32)
+    test_vpc = get_bootstrap_resources().SharedTestVPC
+
+    network_load_balancer = self.elbv2_client.create_load_balancer(
+      Name=self.name,
+      Scheme=self.scheme,
+      Type=self.type,
+      IpAddressType=self.ip_address_type,
+      Subnets=test_vpc.public_subnets.subnet_ids
+    )
+
+    self.arn = network_load_balancer.get("LoadBalancers")[0].get("LoadBalancerArn")
+
+    waiter = self.elbv2_client.get_waiter('load_balancer_available')
+    waiter.wait(LoadBalancerArns=[self.arn])
+
+  def cleanup(self):
+    """Deletes a Network Load Balancer.
+    """
+
+    self.elbv2_client.delete_load_balancer(
+      LoadBalancerArn=self.arn
+    )
+
+    waiter = self.elbv2_client.get_waiter('load_balancers_deleted')
+    waiter.wait(LoadBalancerArns=[self.arn])
+
+    super().cleanup()
