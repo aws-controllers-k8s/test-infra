c55529eb42338ea5db41212f34bd3a2d83ccea3d
Yu-Lang Chu <yu.chu@deliveryhero.com>
2023-04-13 17:10:02 +0200

    add info to run controller

--- scripts/run-ack-dev.sh
+++ scripts/run-ack-dev.sh
@@ -13,6 +13,8 @@ AWS_SERVICE=$(echo "${AWS_SERVICE:-""}" | tr '[:upper:]' '[:lower:]')
 
 CONTROLLER_NAMESPACE=${CONTROLLER_NAMESPACE:-"ack-system"}
 
+CLUSTER_NAME="ack-dev-cluster-$AWS_SERVICE"
+
 source "$SCRIPTS_DIR/lib/aws.sh"
 source "$SCRIPTS_DIR/lib/common.sh"
 source "$SCRIPTS_DIR/lib/config.sh"
@@ -22,24 +24,25 @@ source "$SCRIPTS_DIR/controller-setup.sh"
 source "$SCRIPTS_DIR/kind.sh"
 
 ensure_cluster() {
-    local cluster_create="$(get_cluster_create)"
-    if [[ "$cluster_create" == true ]]; then
-        local cluster_name="ack-dev-cluster-$AWS_SERVICE"
+      info_msg "Creating KIND cluster ..."
+      setup_kind_cluster "$CLUSTER_NAME" "$CONTROLLER_NAMESPACE"
 
-        info_msg "Creating KIND cluster ..."
-        setup_kind_cluster "$cluster_name" "$CONTROLLER_NAMESPACE"
-
-        info_msg "Installing CRDs and RBAC "
-        install_crd_and_rbac "$CONTROLLER_NAMESPACE"
-    else
-        info_msg "Testing connection to existing cluster ..."
-        _ensure_existing_context
-    fi
+      info_msg "Installing CRDs , common and RBAC manifest..."
+      install_crd_and_rbac "$CONTROLLER_NAMESPACE"
 }
 
 run() {
     ensure_aws_credentials
     ensure_cluster
+    local kubeconfig_path="$ROOT_DIR/build/clusters/$CLUSTER_NAME/kubeconfig"
+    info_msg "Before running the controller, you need kubeconfig and aws credentials."
+    info_msg "After executing the above source command to set the kubeconfig to environment, "
+    info_msg "run the following command to start the controller from the controller repo:"
+    echo ""
+    echo "go run ./cmd/controller/main.go --aws-region eu-central-1 --log-level debug --enable-development-logging"
+    echo ""
+    info_msg "if you run the controller from your code editor/IDE, you can set the following environment variables:"
+    echo "KUBECONFIG=$kubeconfig_path"
     exit $?
 }
 
