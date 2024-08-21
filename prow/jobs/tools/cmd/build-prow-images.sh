0353c61d3412477700caffa83781edfe600a5804
michaelhtm <98621731+michaelhtm@users.noreply.github.com>
2024-08-21 13:20:08 -0700

    Fix `build-prow-images` script to use correct publish arn

--- prow/jobs/tools/cmd/build-prow-images.sh
+++ prow/jobs/tools/cmd/build-prow-images.sh
@@ -5,15 +5,21 @@ buildah_login() {
   echo "$__pw" | buildah login -u AWS --password-stdin public.ecr.aws
 }
 
-ECR_PUBLISH_ARN=$(aws ssm get-parameter --name /ack/prow/cd/public_ecr/publish_role --query Parameter.Value --output text 2>/dev/null) || ASSUME_EXIT_VALUE=$?
-if [ "$ASSUME_EXIT_VALUE" -ne 0 ]; then
-  echo "build-prow-images.sh] [SETUP] Could not find the iam role to publish images to public ecr repository"
-  exit 1
+ECR_PUBLISH_ARN=""
+if ! ECR_PUBLISH_ARN="$(aws ssm get-parameter --name /ack/prow/cd/test-infra/publish-prow-images --query Parameter.Value --output text 2>/dev/null)"; then
+    echo "Could not find the IAM role to publish images to the public ECR repository"
+    exit 1
 fi
 export ECR_PUBLISH_ARN
 echo "build-prow-images.sh] [SETUP] exported ECR_PUBLISH_ARN"
 
-ASSUME_COMMAND=$(aws sts assume-role --role-arn $ECR_PUBLISH_ARN --role-session-name 'publish-images' --duration-seconds 3600 | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+ASSUME_COMMAND=$(aws sts assume-role-with-web-identity \
+    --role-arn $ECR_PUBLISH_ARN \
+    --role-session-name "WebIdentitySession" \
+    --web-identity-token "$(cat $AWS_WEB_IDENTITY_TOKEN_FILE)" \
+    --output json \
+    --duration-seconds 3600 \
+    | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 eval $ASSUME_COMMAND
 echo "build-prow-images.sh] [SETUP] Assumed ECR_PUBLISH_ARN"
 
