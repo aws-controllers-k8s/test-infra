2fd98f5b1f8248ba96e4534c68dd3151e53862e0
Iulian Taiatu <itaiatu@adobe.com>
2025-02-05 12:16:45 +0200

    Pass the LOCAL_MODULES env to build-controller-image.sh
    Signed-off-by: Iulian Taiatu <itaiatu@adobe.com>

--- scripts/controller-setup.sh
+++ scripts/controller-setup.sh
@@ -48,9 +48,8 @@ _build_controller_image() {
     local __img_name=$1
 
     local local_build_default="$(get_is_local_build)"
-    LOCAL_MODULES=${LOCAL_MODULES:-$local_build_default}
 
-    AWS_SERVICE_DOCKER_IMG="$__img_name" ${CODE_GENERATOR_SCRIPTS_DIR}/build-controller-image.sh ${AWS_SERVICE} 1>/dev/null
+    LOCAL_MODULES=${LOCAL_MODULES:-$local_build_default} AWS_SERVICE_DOCKER_IMG="$__img_name" ${CODE_GENERATOR_SCRIPTS_DIR}/build-controller-image.sh ${AWS_SERVICE} 1>/dev/null
 }
 
 _load_controller_image() {
