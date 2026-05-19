18b5f6eafd571f36ba2470d9ea116926043c9745
Gustavo Diaz <gustidia@amazon.com>
2026-05-19 01:12:53 +0000

    Replace references to PROW_IMAGES_REPO with PROW_IMAGES_REPO_URI

--- prow/jobs/images/push-image.sh
+++ prow/jobs/images/push-image.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 VERSION=${VERSION:-$(git describe --tags --always --dirty || echo "unknown")}
 
-DEFAULT_DOCKER_REPOSITORY="${PROW_IMAGE_REPO:-}"
+DEFAULT_DOCKER_REPOSITORY="${PROW_IMAGES_REPO_URI:-}"
 DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-"$DEFAULT_DOCKER_REPOSITORY"}
 
 QUIET=${QUIET:-"false"}
